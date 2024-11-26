f5348bea8a7ad13212cdded99cb41da69f38158f
shubhswain45 <shubhkantswain@.com>
2024-11-26 16:33:47 +0530

    kkk

--- app/server/page.tsx
+++ app/server/page.tsx
@@ -1,12 +1,12 @@
+import { cookies } from "next/headers";
 
-export default function Home() {
-    const cookieStore = cookies();
+export default async function Home() {
+    const cookieStore = await cookies();
     const dummyCookie = cookieStore.get('dummyCookie');
 
     return (
         <div>
             <h1>Cookie Demo</h1>
-            <SetCookieButton />
             <div style={{ marginTop: '20px' }}>
                 <h2>Cookie from Server Component:</h2>
                 <pre>{JSON.stringify(dummyCookie, null, 2)}</pre>
